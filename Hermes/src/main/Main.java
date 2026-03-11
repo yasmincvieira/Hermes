7d788306cdc882dba6dde8807b31c18c55893eda
aluno <aluno@10.61.7.34>
2026-03-11 10:14:33 -0300

    commit Yasmin 05

--- Hermes/src/main/Main.java
+++ Hermes/src/main/Main.java
@@ -1,6 +1,13 @@
 package main;
 
+import controller.CadastroController;
+import controller.LoginController;
+import controller.Navegador;
+import models.UsuarioDAO;
 import view.Janela;
+import view.TelaCadastro;
+import view.TelaInicial;
+import view.TelaLogin;
 
 public class Main {
 
@@ -8,6 +15,23 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		Janela janela = new Janela();
+		Navegador navegador = new Navegador(janela);
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+		TelaCadastro telaCadastro = new TelaCadastro();
+		CadastroController cadastroController = new CadastroController();
+		
+		TelaInicial telaInicial = new TelaInicial();
+		
+		TelaLogin telaLogin = new TelaLogin();
+		LoginController loginController = new LoginController();
+		//telaLogin.adicionarOuvinte(loginController);
+
+		
+
+		navegador.adicionarPainel("CADASTRO", telaCadastro);
+		navegador.adicionarPainel("LOGIN", telaLogin);
+		navegador.adicionarPainel("INICIO", telaInicial);
 		
 
 	}
